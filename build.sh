2f2f8f33280887cbc3708cf3e796886336bb4e8e
rdeekshitha184-ship-it <rdeekshitha184@gmail.com>
2026-04-20 11:37:32 +0530

    Auto create admin user on deploy

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
+# set -o errexit
+# pip install -r requirements.txt
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from accounts.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@lms.com', 'Admin@1234', role='teacher', is_class_teacher=True)
+    print('Admin created!')
+else:
+    print('Admin already exists!')
+"
