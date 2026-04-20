e1f5111678509bb762516e82f936077e6b91be37
rdeekshitha184-ship-it <rdeekshitha184@gmail.com>
2026-04-20 14:52:21 +0530

    Fix static files and admin CSS

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
+# # # set -o errexit
+# # # pip install -r requirements.txt
+# # # python manage.py collectstatic --no-input
+# # # python manage.py migrate
+
 # # set -o errexit
 # # pip install -r requirements.txt
 # # python manage.py collectstatic --no-input
 # # python manage.py migrate
+# # python manage.py shell -c "
+# # from accounts.models import User
+# # if not User.objects.filter(username='admin').exists():
+# #     User.objects.create_superuser('admin', 'admin@lms.com', 'Admin@1234', role='teacher', is_class_teacher=True)
+# #     print('Admin created!')
+# # else:
+# #     print('Admin already exists!')
+# # "
 
 # set -o errexit
 # pip install -r requirements.txt
-# python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input --clear
 # python manage.py migrate
 # python manage.py shell -c "
 # from accounts.models import User
@@ -15,10 +28,10 @@
 # else:
 #     print('Admin already exists!')
 # "
-
 set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --clear
+mkdir -p staticfiles
+python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py shell -c "
 from accounts.models import User
